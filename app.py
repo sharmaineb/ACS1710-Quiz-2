1b0bc74a633799d0290d29394fec342de05be4d7
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-11-19 18:21:29 -0800

    saving progress

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ import json
 
 app = Flask(__name__)
 
-API_URL = 'https://swapi.py4e.com/api/'
+API_URL = "https://swapi.py4e.com/api/"
 
-@app.route('/')
+@app.route("/")
 def homepage():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/search_results', methods = ['GET', 'POST'])
-def search_reults():
-    if request.method == 'POST':
+@app.route("/search_results")
+def results():
+    if request.method == "POST":
         people = request.form.get('people_list')
-        people_result = requests.get(f"{API_URL} people/ {people}")
+        people_result = requests.get(f"{API_URL}people/{people}")
         name = json.loads(people_result.content).get('name')
         height = json.loads(people_result.content).get('height')
         mass = json.loads(people_result.content).get('mass')
@@ -28,7 +28,11 @@ def search_reults():
             'hair_color' : hair_color,
             'eye_color' : eye_color
         }
-    return render_template('search_results')
+
+        return render_template("search_results.html", **context)
+
+    else:
+        return render_template("search_results.html")
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'

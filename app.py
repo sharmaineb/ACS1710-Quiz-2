1cf39b655c9483e6c0edd86f8687d27500516eff
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-11-21 17:55:48 -0800

    completed quiz 2

--- app.py
+++ app.py
@@ -6,14 +6,18 @@ app = Flask(__name__)
 
 API_URL = "https://swapi.py4e.com/api/"
 
+# Homepage
 @app.route("/")
 def homepage():
     return render_template("home.html") 
 
+# Shows the user a form to get character's results 
 @app.route("/results")
 def form():
     return render_template("results.html")
 
+# Shows the results ot the character the user searched for
+# from SWAPI 
 @app.route("/results", methods=["GET", "POST"])
 def results():
     if request.method == "POST":
@@ -26,12 +30,29 @@ def results():
         hair_color = json.loads(character_result.content).get('hair_color')
         eye_color = json.loads(character_result.content).get('eye_color')
 
+        # homeworld
+        # attempted homeworld stretch challenge
+        # homeworld = request.form.get('planet')
+        # homeworld_input = requests.get(f"{API_URL}planets/{homeworld}")
+        # homeworld_search = json.loads(homeworld.content).get('homeworld_search')
+        
+        # films
+        # my attempt at the film stretch challenge, but it still returns the url of the film :)
+        films = json.loads(character_result.content).get('films')
+        list_of_films = []
+        for film in films:
+            film = requests.get(f"{API_URL}films/{film}")
+            film_title = json.loads(character_result.content).get('film_title')
+            list_of_films.append(film_title)
+
+        # key value pairs
         context = {
             'name' : name,
             'height' : height,
             'mass' : mass,
             'hair_color' : hair_color,
-            'eye_color' : eye_color
+            'eye_color' : eye_color,
+            'films' : films
         }
 
         return render_template("results.html", **context)
